bb66e3d297abc99232199758cad44ec18485f32b
BryanPalmeiras <168776218+Toddynxz@users.noreply.github.com>
2026-03-30 08:57:19 -0300

    atualização 30/03/2026

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ $resultado = mysqli_query($conexao, $sql);
         <form action ="salvar.php" method = "post">
             <label>Nome:</label>
             <input type = "text" name = "nome" required>
+            <label>Preço:</label>
             <input type = "number" name = "preco" required>
+            <label>Categoria:</label>
+            <input type = "text" name = "Categoria" required>
+            <button type= "submit">Salvar</button>
         </form>
     </body>
 </html>
